41cfb2bd889c320b62a8ea4cc0ea450039ec06eb
vincent-zola <vincent.del@protonmail.com>
2022-04-16 05:28:09 +0200

    Added Input from App-Start

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Input.css";
 
-const Input = ({
-  text,
-  result,
-  calculation,
-  setText,
-  calculateResult,
-  
-}) => {
+const Input = ({ text, result, calculation, setText, calculateResult }) => {
   // If Enter is pressed in Input field than calculate result
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -16,17 +9,39 @@ const Input = ({
     }
   };
 
+  // * Keypress handler functions
+  // grab Input element with useRef
+  let calculationInput = useRef();
+
+  // listen to any keypress
+  useEffect(() => {
+    // if any key is pressed focus on Input
+    const addToTextKeyboard = () => {
+      calculationInput.current.focus();
+    };
+    document.addEventListener("keydown", addToTextKeyboard);
+    // cleanup function
+    return () => {
+      document.removeEventListener("keydown", addToTextKeyboard);
+    };
+  }, []);
+
   return (
     <div className="input-wrapper">
       <div className="result">
         {/* Show calculation from input at the top of the screen */}
-        {calculation && <p onClick={() => setText(calculation)} >({calculation})</p>}
+        {calculation && (
+          <p onClick={() => setText(calculation)}>({calculation})</p>
+        )}
         {/* Show result in green or Error-text in red depending if result is a number or not */}
         {isNaN(result) ? (
           <h1 className="error-text">{result}</h1>
         ) : (
-          // onClick copy result to Input 
-          <h1 onClick={() => setText((result).toString())} className="result-text">
+          // onClick copy result to Input
+          <h1
+            onClick={() => setText(result.toString())}
+            className="result-text"
+          >
             {result}
           </h1>
         )}
@@ -38,6 +53,15 @@ const Input = ({
         placeholder="|"
         type="text"
         onKeyPress={handleKeyPress}
+        // grab Input element with useRef
+        ref={calculationInput}
+        // ref triggers focus, and that triggers onFocus to calculate the end and set the cursor accordingly.
+        onFocus={(e) =>
+          e.target.setSelectionRange(
+            e.target.value.length,
+            e.target.value.length
+          )
+        }
         // By start of the App Input will be focused, cursor blinks, user can type immediately, disabled because bad at phones
         // autoFocus
         // onBlur={({ target }) => target.focus()}
